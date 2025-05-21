272bf4be3bd161088b4b54562aceee8f3c57c64e
Nick <n.sakharov@icloud.com>
2025-05-22 00:54:40 +0700

    Finally got css to work

--- tic-tac-toe/src/app/square/square.component.ts
+++ tic-tac-toe/src/app/square/square.component.ts
@@ -1,9 +1,9 @@
-import { NgFor, NgIf } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  imports: [NgIf, NgFor],
+  imports: [NgIf],
   templateUrl: './square.component.html',
   styleUrl: './square.component.css',
 })
