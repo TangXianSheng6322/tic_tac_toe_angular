54e763110a8bdef410bd65039fd5343e4c1820ea
Nick <n.sakharov@icloud.com>
2025-05-21 22:58:15 +0700

    Done with functions in tictactoe component

--- tic-tac-toe/src/app/tictactoe/tictactoe.component.ts
+++ tic-tac-toe/src/app/tictactoe/tictactoe.component.ts
@@ -25,7 +25,37 @@ export class TictactoeComponent implements OnInit {
     this.newGame();
   }
 
-  makeAMove() {
-    this.xIsNext;
+  makeAMove(idx: number) {
+    if (!this.squares[idx]) {
+      this.squares.splice(idx, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+    }
+    return this.caculateWinner();
+  }
+
+  caculateWinner() {
+    const winCombo = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winCombo.length; i++) {
+      const [a, b, c] = winCombo[i];
+
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
   }
 }
